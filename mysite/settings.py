529f60c9748c4f6a4a7b11ed66b2187e0e15444d
Rono <peter@copiakenya.com>
2022-04-10 06:55:47 +0300

    removed passwords

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4ia!!u@txnpy2tp6$99&4%)3c1bjam@8prdcupc&$wgc0v1v4('
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'hash_tech',
-        'USER': 'root',
-        'PASSWORD': 'rono',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
 
